fe586d35dc8752b697c99b7ea14cc06d9ed6c4a5
Corban Travis <98620032+CorbOnTheCobb@users.noreply.github.com>
2025-11-11 18:43:40 -0600

    Uploaded front-end content.
    A basic layout for the home page and favorites page made in React. 
    
    TODO: Add the login/account registration/password recovery page and use of an API to get data about movies.

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import MovieCard from "../components/MovieCard"
+import {useState} from "react"
+import "../css/Home.css"
+
+function Home() {
+    const[searchQuery, setSearchQuery] = useState("");
+
+    const movies = [
+        {id: 1, title: "Random Movie 1", release_date: "2021"},
+        {id: 2, title: "Random Movie 2", release_date: "2022"},
+        {id: 3, title: "Random Movie 3", release_date: "2023"},
+    ];
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+    }
+
+    return <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input
+                type="text"
+                placeholder="Search for movies..."
+                className="search-input"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button type="submit" className="search-button">Search</button>
+        </form>
+
+        <div className="movies-grid">
+            {movies.map((movie) => (
+                movie.title.toLowerCase().startsWith(searchQuery.toLowerCase()) && <MovieCard movie={movie} key={movie.id} />
+        ))}
+        </div>
+    </div>
+}
+
+export default Home
