1d243e377dba28438db1b2b62f9660c3f6a47385
KilchristCarson <kilchristcarson12@gmail.com>
2025-11-11 23:47:14 -0600

    Frontend w/ Movie Detail
    Added MovieDetail to the frontend, with routing via clicking the Movie Card
    
    Add the login/account registration/password recovery page and use of an API to get data about movies. Review Page

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,25 +1,36 @@
 import "../css/MovieCard.css"
+import { useNavigate } from "react-router-dom";
 
 function MovieCard({movie}) {
+    const navigate = useNavigate();
 
-    function onClickFavorite() {
-        alert("clicked")
+    function onClickFavorite(e) {
+        alert("clicked favorite")
     }
 
-    return <div className="movie-card">
-        <div className="movie-poster">
-            <img src={movie.url} alt={movie.title}/>
-            <div className={"movie-overlay"}>
-                <button className="favorite-btn" onClick={onClickFavorite}>
-                    ♡
-                </button>
+     function onClickMovie(e) {
+    
+        setTimeout(() => {
+            navigate(`/movie/${movie.id}`);
+        }, 0);
+    }
+
+    return (
+        <div className="movie-card" onClick={onClickMovie}>
+            <div className="movie-poster">
+                <img src={movie.url} alt={movie.title}/>
+                <div className={"movie-overlay"}>
+                    <button className="favorite-btn" onClick={onClickFavorite}>
+                        ♡
+                    </button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
             </div>
         </div>
-        <div className="movie-info">
-            <h3>{movie.title}</h3>
-            <p>{movie.release_date}</p>
-        </div>
-    </div>
+    )
 }
 
 export default MovieCard
